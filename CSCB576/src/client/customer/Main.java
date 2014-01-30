3b1987a92a26e460c9f312983c09407df7b69b5c
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-01-31 01:28:20 +0200

    Add customer main class and startup script

--- CSCB576/src/client/customer/Main.java
+++ CSCB576/src/client/customer/Main.java
@@ -1,9 +1,27 @@
 package client.customer;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import server.IAppStoreServer;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("The client customer");
+		if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            String name = "AppStore";
+            Registry registry = LocateRegistry.getRegistry(args[0]);
+            IAppStoreServer comp = (IAppStoreServer) registry.lookup(name);
+            int status = comp.start();
+            System.out.println(status);
+        } catch (Exception e) {
+            System.err.println("AppServer exception:");
+            e.printStackTrace();
+        }
 	}
 
 	
