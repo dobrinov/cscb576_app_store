040e4e39d0a9158fe92402f8711e72f90c0f423a
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-01-31 01:26:57 +0200

    Add hostname to the server startup script

--- CSCB576/server.bat
+++ CSCB576/server.bat
@@ -1,4 +1,4 @@
 set CLASSPATH=src;build\jar\CSCB576.jar
 start rmiregistry
 
-java -Djava.security.policy=src/server/server.policy server.AppStoreServer
+java -Djava.security.policy=src/server/server.policy -Djava.rmi.server.hostname=localhost server.AppStoreServer
