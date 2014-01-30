a536eec4cb3e3d3bbc2c3553cffca2de1982538c
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-01-31 01:30:15 +0200

    Fix identation

--- CSCB576/src/server/ServerState.java
+++ CSCB576/src/server/ServerState.java
@@ -7,23 +7,23 @@ public enum ServerState {
 
 	STARTED {
 		@Override
-        public Set<ServerState> possibleFollowUps() {
-            return EnumSet.of(STOPPED, PAUSED);
-        }
+		public Set<ServerState> possibleFollowUps() {
+		    return EnumSet.of(STOPPED, PAUSED);
+		}
 	},
 	
 	STOPPED {
 		@Override
-        public Set<ServerState> possibleFollowUps() {
-            return EnumSet.of(STARTED);
-        }
+		public Set<ServerState> possibleFollowUps() {
+		    return EnumSet.of(STARTED);
+		}
 	},
 	
 	PAUSED {
 		@Override
-        public Set<ServerState> possibleFollowUps() {
-            return EnumSet.of(STARTED);
-        }
+		public Set<ServerState> possibleFollowUps() {
+		    return EnumSet.of(STARTED);
+		}
 	};
 	
 	public Set<ServerState> possibleFollowUps() {
